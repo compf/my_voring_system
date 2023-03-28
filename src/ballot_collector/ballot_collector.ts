65eb06747f53fd482458d74090fc535de0e34459
=compf <=>
2023-03-28 19:45:08 +0200

    ... for collector

--- src/ballot_collector/ballot_collector.ts
+++ src/ballot_collector/ballot_collector.ts
@@ -1,7 +1,6 @@
 import express from "express";
 import https from "https";
 import fs, { readFileSync } from "fs";
-const db = require('better-sqlite3')('database/database.db');
 import crypto from "crypto";
 import ejs from "ejs";
 let finnished={finnished:false};
@@ -16,14 +15,12 @@ import { HttpsServerChannel } from "../util/https_channel";
 import { SQLLiteDataService } from "../util/sqlite_data_service";
 const app = express();
 app.use(express.urlencoded({extended: true}));
-console.log(__dirname+'/database.db');
 const BALLOT_COLLECTOR_PORT=3002;
 export class BallotCollectorService implements DistributedServerService{
   channel:CommunicationChannel;
   dataService:DataService;
    isValidBallotRequest(row:any):boolean{
-    const issuedCountStatement=db.prepare("SELECT COUNT(uuid) FROM BallotsIssued WHERE uuid=?",row.id);
-    let cnt=(issuedCountStatement.get(0) as any).count
+    let cnt=this.dataService.count("BallotsIssued",(r)=>r["uuid"]==row.id);
     console.log(cnt)
     if(cnt>0){
       return false;
@@ -32,14 +29,14 @@ export class BallotCollectorService implements DistributedServerService{
   }
   run(): void {
     const isValidBallotRequest=this.isValidBallotRequest;
+    let dataService=this.dataService;
     this.channel.registerEvent("/submitBallot",HttpMethod.Post, function (request, response) {
       console.log("request",request.body);
       const b=(request.body["ballot"]);
       const body=fromJSON(b,false);
       console.log(body);
       
-      var stmt=db.prepare("SELECT * FROM BallotAuthorization ");
-       for( let row of stmt.iterate()){
+       for( let row of dataService.queryAll ("BallotAuthorization")){
         if(finnished.finnished){
           return false;
         }
@@ -48,7 +45,7 @@ export class BallotCollectorService implements DistributedServerService{
     
         console.log("hash",row.id,compareHash);
         console.log()
-        const timeDiff=body.issueTime.getTime()-row.time;
+        const timeDiff=body.issueTime.getTime()-parseInt(row.time);
        // console.log(compareHash);
         if(compareHash==row.id &&  isValidBallotRequest(row)){
          
