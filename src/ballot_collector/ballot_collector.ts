c03961db680e18dcd5a5821a2f862f3afc8da2f7
=compf <=>
2023-03-28 19:40:54 +0200

    updated ballot collector to have the same abstraction as the others

--- src/ballot_collector/ballot_collector.ts
+++ src/ballot_collector/ballot_collector.ts
@@ -9,76 +9,84 @@ import { AuthorizationInformation } from "../common/authorization_information";
 import { BallotRequest } from "../common/ballot_request";
 import { Ballot, fromJSON } from "../common/ballot";
 import { time } from "console";
+import { DistributedServerService } from "../util/distributed_server_service";
+import { CommunicationChannel, HttpMethod } from "../util/communication_channel";
+import { DataService } from "../util/data_service";
+import { HttpsServerChannel } from "../util/https_channel";
+import { SQLLiteDataService } from "../util/sqlite_data_service";
 const app = express();
 app.use(express.urlencoded({extended: true}));
 console.log(__dirname+'/database.db');
 const BALLOT_COLLECTOR_PORT=3002;
-
-app.post("/submitBallot", function (request, response) {
-  console.log("request",request.body);
-  const b=(request.body["ballot"]);
-  const body=fromJSON(b,false);
-  console.log(body);
-  
-  var stmt=db.prepare("SELECT * FROM BallotAuthorization ");
-   for( let row of stmt.iterate()){
-    if(finnished.finnished){
+export class BallotCollectorService implements DistributedServerService{
+  channel:CommunicationChannel;
+  dataService:DataService;
+   isValidBallotRequest(row:any):boolean{
+    const issuedCountStatement=db.prepare("SELECT COUNT(uuid) FROM BallotsIssued WHERE uuid=?",row.id);
+    let cnt=(issuedCountStatement.get(0) as any).count
+    console.log(cnt)
+    if(cnt>0){
       return false;
     }
-    console.log("both",body.uuid+row.salt)
-    const compareHash=crypto.createHash("sha256").update(body.uuid+row.salt).digest("base64");
-
-    console.log("hash",row.id,compareHash);
-    console.log()
-    const timeDiff=body.issueTime.getTime()-row.time;
-   // console.log(compareHash);
-    if(compareHash==row.id && isValidBallotRequest(row)){
-     
-      response.write("Successful");
-      response.end();
-      console.log("compared")
-      finnished.finnished=true;
-      console.log("we",finnished.finnished)
+    return true;
+  }
+  run(): void {
+    const isValidBallotRequest=this.isValidBallotRequest;
+    this.channel.registerEvent("/submitBallot",HttpMethod.Post, function (request, response) {
+      console.log("request",request.body);
+      const b=(request.body["ballot"]);
+      const body=fromJSON(b,false);
+      console.log(body);
       
-  
+      var stmt=db.prepare("SELECT * FROM BallotAuthorization ");
+       for( let row of stmt.iterate()){
+        if(finnished.finnished){
+          return false;
+        }
+        console.log("both",body.uuid+row.salt)
+        const compareHash=crypto.createHash("sha256").update(body.uuid+row.salt).digest("base64");
+    
+        console.log("hash",row.id,compareHash);
+        console.log()
+        const timeDiff=body.issueTime.getTime()-row.time;
+       // console.log(compareHash);
+        if(compareHash==row.id &&  isValidBallotRequest(row)){
+         
+          response.write("Successful");
+          response.end();
+          console.log("compared")
+          finnished.finnished=true;
+          console.log("we",finnished.finnished)
+          
+      
+         
+    
+        }
+      };
+      console.log("finn",finnished.finnished)
+      if(!finnished.finnished){
+        console.log("cool");
+        response.send("NOT SUCCESSFUL");
+        response.end();
+      }
+    
      
-
-    }
-  };
-  console.log("finn",finnished.finnished)
-  if(!finnished.finnished){
-    console.log("cool");
-    response.send("NOT SUCCESSFUL");
-    response.end();
+    });
+  }
+  constructor(channel:CommunicationChannel,dataService:DataService){
+    this.channel=channel;
+    this.dataService=dataService;
   }
 
- 
-});
-function isValidBallotRequest(row:any):boolean{
-  const issuedCountStatement=db.prepare("SELECT COUNT(uuid) FROM BallotsIssued WHERE uuid=?",row.id);
-  let cnt=(issuedCountStatement.get(0) as any).count
-  console.log(cnt)
-  if(cnt>0){
-    return false;
-  }
-  return true;
 }
 
-const pki_path=__dirname+"/pki/"
-https
-  .createServer(
-    {
-      key: fs.readFileSync(pki_path+"ballot_collector.key.pem"),
-      cert: fs.readFileSync(pki_path+"ballot_collector.cert.pem"),
-      
-      requestCert: false,
-      ca: 
-        fs.readFileSync(`pki/my_ca-crt.pem`),
-    },
-    app
-  )
-  .listen(BALLOT_COLLECTOR_PORT, function () {
-    console.log(
-      "Example app listening on port 3002! Go to https://localhost:3002/"
-    );
-  });
+
+if(require.main){
+  const pki_path=__dirname+"/pki/"
+  let channel=new HttpsServerChannel(BALLOT_COLLECTOR_PORT,pki_path+"ballot_collector.key.pem",pki_path+"ballot_collector.cert.pem",false);
+  let dataService=new SQLLiteDataService();
+  let service=new BallotCollectorService(channel,dataService);
+  service.run();
+}
+
+
